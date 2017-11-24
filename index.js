2b581009d326e3c3e10449e4152b75701aad70fb
Mu-An ✌️ Chiou <muan@github.com>
2017-11-24 15:47:45 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+class DetailsDialogElement extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({mode: 'open'})
+    this.shadowRoot.innerHTML = `
+      <style>
+        .close-button {
+          background: none;
+          position: absolute;
+          right: 0;
+          top: 0;
+          padding: 20px;
+          border: 0;
+          line-height: 1;
+        }
+      </style>
+      <slot></slot>
+      <button type="button" class="close-button" aria-label="Close dialog" data-close-dialog>&#9587;</button>
+    `
+    this.details = this.parentElement
+    this.closeButton = this.shadowRoot.querySelector('.close-button')
+
+    const keyDownHelpers = this._keyDownHelpers.bind(this)
+    const captureDismissal = this._captureDismissal.bind(this)
+
+    this.details.addEventListener(
+      'toggle',
+      function() {
+        if (this.details.open) {
+          this._autofocus()
+          this.details.addEventListener('keydown', keyDownHelpers)
+          this.shadowRoot.addEventListener('click', captureDismissal)
+        } else {
+          const summary = this.details.querySelector('summary')
+          summary.focus()
+
+          this.details.removeEventListener('keydown', keyDownHelpers)
+          this.shadowRoot.removeEventListener('click', captureDismissal)
+        }
+      }.bind(this),
+      {capture: true}
+    )
+  }
+
+  connectedCallback() {
+    this.setAttribute('role', 'dialog')
+  }
+
+  _autofocus() {
+    let autofocus = this.querySelector('[autofocus]')
+    if (!autofocus) {
+      autofocus = this
+      this.setAttribute('tabindex', '-1')
+    }
+    autofocus.focus()
+  }
+
+  _captureDismissal(event) {
+    if (event.target.hasAttribute('data-close-dialog')) {
+      this.details.open = false
+    }
+  }
+
+  _keyDownHelpers(event) {
+    if (event.key === 'Escape') {
+      event.currentTarget.open = false
+    } else if (event.key === 'Tab') {
+      this._restrictTabBehavior(event)
+    }
+  }
+
+  _restrictTabBehavior(event) {
+    event.preventDefault()
+
+    const modal = event.currentTarget
+    const elements = Array.from(modal.querySelectorAll('a, input, button, textarea')).filter(function(element) {
+      // Need visible check
+      return !element.disabled
+    })
+
+    elements.unshift(this.closeButton)
+    const movement = event.shiftKey ? -1 : 1
+    const currentFocus = elements.filter(el => el.matches(':focus'))[0]
+    let targetIndex = elements.length - 1
+
+    if (currentFocus) {
+      const currentIndex = elements.indexOf(currentFocus)
+      if (currentIndex !== -1) {
+        const newIndex = currentIndex + movement
+        if (newIndex >= 0) targetIndex = newIndex % elements.length
+      }
+    }
+
+    elements[targetIndex].focus()
+  }
+}
+
+window.customElements.define('details-dialog', DetailsDialogElement)
