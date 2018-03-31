2d17a7e38a94e94f21f3daacf4cf912247096468
David Graham <david.malcom.graham@gmail.com>
2018-03-30 18:13:57 -0600

    Add test suite

---
+++ test/test.js
@@ -0,0 +1,35 @@
+describe('details-dialog-element', function() {
+  describe('element creation', function() {
+    it('creates from document.createElement', function() {
+      const el = document.createElement('details-dialog')
+      assert.equal('DETAILS-DIALOG', el.nodeName)
+    })
+
+    it('creates from constructor', function() {
+      const el = new window.DetailsDialogElement()
+      assert.equal('DETAILS-DIALOG', el.nodeName)
+    })
+  })
+
+  describe('after tree insertion', function() {
+    beforeEach(function() {
+      const container = document.createElement('div')
+      container.innerHTML = `
+      <details>
+        <summary>Click</summary>
+        <details-dialog>
+          <p>Hello</p>
+        </details-dialog>
+      </details>`
+      document.body.append(container)
+    })
+
+    afterEach(function() {
+      document.body.innerHTML = ''
+    })
+
+    it('creates a close button', function() {
+      assert(document.querySelector('details-dialog button[data-close-dialog]'))
+    })
+  })
+})
