f4b0a51c89bba982e2188c749eea0ac05772668c
Kevin Sawicki <kevinsawicki@gmail.com>
2018-09-12 15:36:08 -0700

    Add toggle tests when no summary element is present

--- test/test.js
+++ test/test.js
@@ -14,6 +14,11 @@ describe('details-dialog-element', function() {
   })
 
   describe('after tree insertion', function() {
+    let details
+    let dialog
+    let summary
+    let close
+
     beforeEach(function() {
       const container = document.createElement('div')
       container.innerHTML = `
@@ -26,6 +31,11 @@ describe('details-dialog-element', function() {
         </details>
       `
       document.body.append(container)
+
+      details = document.querySelector('details')
+      dialog = details.querySelector('details-dialog')
+      summary = details.querySelector('summary')
+      close = dialog.querySelector(CLOSE_SELECTOR)
     })
 
     afterEach(function() {
@@ -33,8 +43,6 @@ describe('details-dialog-element', function() {
     })
 
     it('toggles open', function() {
-      const details = document.querySelector('details')
-      const dialog = details.querySelector('details-dialog')
       assert(!details.open)
       dialog.toggle(true)
       assert(details.open)
@@ -43,9 +51,6 @@ describe('details-dialog-element', function() {
     })
 
     it('closes with close button', function() {
-      const details = document.querySelector('details')
-      const dialog = details.querySelector('details-dialog')
-      const close = dialog.querySelector(CLOSE_SELECTOR)
       assert(!details.open)
       dialog.toggle(true)
       assert(details.open)
@@ -54,9 +59,6 @@ describe('details-dialog-element', function() {
     })
 
     it('closes when summary is clicked', function() {
-      const details = document.querySelector('details')
-      const summary = document.querySelector('summary')
-      const dialog = details.querySelector('details-dialog')
       assert(!details.open)
       dialog.toggle(true)
       assert(details.open)
@@ -65,8 +67,6 @@ describe('details-dialog-element', function() {
     })
 
     it('closes when escape key is pressed', async function() {
-      const details = document.querySelector('details')
-      const dialog = details.querySelector('details-dialog')
       assert(!details.open)
       dialog.toggle(true)
       await waitForToggleEvent(details)
@@ -75,12 +75,7 @@ describe('details-dialog-element', function() {
       assert(!details.open)
     })
 
-    it('supports canceling requests to close the dialog', async function() {
-      const details = document.querySelector('details')
-      const summary = document.querySelector('summary')
-      const dialog = details.querySelector('details-dialog')
-      const close = dialog.querySelector(CLOSE_SELECTOR)
-
+    it('supports canceling requests to close the dialog when a summary element is present', async function() {
       dialog.toggle(true)
       await waitForToggleEvent(details)
       assert(details.open)
@@ -118,6 +113,37 @@ describe('details-dialog-element', function() {
       allowCloseToHappen = true
       close.click()
       assert(!details.open)
+    })
+
+    describe('when no summary element is present', function() {
+      beforeEach(function() {
+        summary.remove()
+      })
+
+      it('toggles open', function() {
+        assert(!details.open)
+        dialog.toggle(true)
+        assert(details.open)
+        dialog.toggle(false)
+        assert(!details.open)
+      })
+
+      it('closes with close button', function() {
+        assert(!details.open)
+        dialog.toggle(true)
+        assert(details.open)
+        close.click()
+        assert(!details.open)
+      })
+
+      it('closes when escape key is pressed', async function() {
+        assert(!details.open)
+        dialog.toggle(true)
+        await waitForToggleEvent(details)
+        assert(details.open)
+        pressEscape(details)
+        assert(!details.open)
+      })
     })
   })
 })
