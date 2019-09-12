778f8f4b7eeca9479b87f160cc880daabefceb2c
Keith Cirkel <keithamus@users.noreply.github.com>
2019-09-12 10:08:27 +0100

    types(ts): add window declaration

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 export default class DetailsDialogElement extends HTMLElement {
   toggle(open: boolean): void;
 }
+
+declare global {
+  interface Window {
+    DetailsDialogElement: DetailsDialogElement
+  }
+}
