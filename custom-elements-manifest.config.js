07fdac55e7a94a61df2251a02620b03b1610b47e
Kristján Oddsson <koddsson@gmail.com>
2022-08-15 11:02:16 +0200

    Use `@github/cem-plugin-readme` instead of inlined version

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,5 +1,5 @@
 import {generateCustomData} from 'cem-plugin-vs-code-custom-data-generator'
-import {readme} from './cem-plugin-readme.js'
+import {readme} from '@github/cem-plugin-readme'
 
 export default {
   packagejson: true,
